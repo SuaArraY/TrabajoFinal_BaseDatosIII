78654b9ab7b7f013c736f19c69c5fddfd74a832a
Yossec SA <yosec.arratea22@gmail.com>
2022-12-02 23:37:46 -0500

    procedimiento del ejercicio 3

--- db_buenasalud.sql
+++ db_buenasalud.sql
@@ -162,3 +162,24 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+
+-- ejercicio 3
+-- Crear un procedimiento que recupere el promedio de edad de las personas por cada departamento.
+
+ USE `db_buenasalud`;
+DROP procedure IF EXISTS `mostrar_promedio_edad_departamento`;
+
+DELIMITER $$
+USE `db_buenasalud`$$
+CREATE PROCEDURE `mostrar_promedio_edad_departamento`()
+BEGIN
+SELECT
+departamento.dpt_Nombre AS `Departamento`,
+AVG(YEAR(CURRENT_DATE)- YEAR(empleado.fecha_nacimiento)) AS `Promedio Edad`
+FROM
+departamento
+INNER JOIN empleado ON empleado.dept_no = departamento.nro_Departamento
+GROUP BY
+departamento.dpt_Nombre;
+END$$

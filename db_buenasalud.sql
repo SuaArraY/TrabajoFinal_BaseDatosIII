ff72bf66f82f8e92ff0744141ef0c1cefd1d229c
Yossec SA <yosec.arratea22@gmail.com>
2022-12-02 23:44:56 -0500

    procedimiento del ejercicio 4

--- db_buenasalud.sql
+++ db_buenasalud.sql
@@ -183,3 +183,24 @@ INNER JOIN empleado ON empleado.dept_no = departamento.nro_Departamento
 GROUP BY
 departamento.dpt_Nombre;
 END$$
+
+-- ejercicio 4
+-- Crear un procedimiento para devolver el apellido, oficio y salario, pasándole 
+-- como parámetro el número del empleado. 
+
+USE `db_buenasalud`;
+DROP procedure IF EXISTS `datos_empleado`;
+
+DELIMITER $$
+USE `db_buenasalud`$$
+CREATE PROCEDURE `datos_empleado`(IN numero_empleado INT)
+BEGIN
+	SELECT 
+		emp_no, apellido, oficio, salario 
+    FROM
+		empleado
+	WHERE 
+		emp_no = numero_empleado;
+END$$
+
+DELIMITER ;

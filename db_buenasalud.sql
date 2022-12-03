b441537db9e9a5d48cc94ff4ace680a6778f7fb9
Yossec SA <yosec.arratea22@gmail.com>
2022-12-02 23:26:10 -0500

    procedimiento del ejercicio 2

--- db_buenasalud.sql
+++ db_buenasalud.sql
@@ -139,3 +139,26 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+
+-- ejercicio 2
+-- Crear un procedimiento almacenado que permita insertar un nuevo departamento
+
+USE `db_buenasalud`;
+DROP procedure IF EXISTS `insertar_nuevo_departamento`;
+
+DELIMITER $$
+USE `db_buenasalud`$$
+CREATE PROCEDURE `insertar_nuevo_departamento`(IN nuevo_departamento VARCHAR(100))
+BEGIN
+	-- sentencia para registrar
+	INSERT INTO 
+		departamento (dpt_Nombre)
+    VALUES
+		(nuevo_departamento);
+	
+	-- sentencia para revisar el dato ingresado
+	SELECT nro_Departamento, dpt_Nombre FROM departamento order by nro_Departamento DESC LIMIT 5;
+END$$
+
+DELIMITER ;

3cb39e8441241fd93ea96d512424c9fb1c39c418
Yossec SA <yosec.arratea22@gmail.com>
2022-12-02 22:17:30 -0500

    creacion de la tabla paciente

--- db_buenasalud.sql
+++ db_buenasalud.sql
@@ -1,3 +1,16 @@
 -- creacion de la base de datos
 CREATE DATABASE db_buenasalud;
 USE db_buenasalud;
+
+-- creacion de las tablas paciente, departamento y empleado
+
+-- Tabla paciente
+CREATE TABLE paciente (
+   inscripcion INT NOT NULL AUTO_INCREMENT,
+   apellido varchar(255) NOT NULL,
+   direccion varchar(1000) NULL,
+   fecha_nac date NOT NULL,
+   sexo char(1) NOT NULL,
+   nss varchar(255) NOT NULL,
+   PRIMARY KEY (`inscripcion`)
+ ) 

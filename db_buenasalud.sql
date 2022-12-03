b24522a4f81bfc4d7096870171a0a789a93ba9f4
Yossec SA <yosec.arratea22@gmail.com>
2022-12-02 23:58:25 -0500

    procedimiento del ejercicio 5

--- db_buenasalud.sql
+++ db_buenasalud.sql
@@ -204,3 +204,24 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+-- ejercicio 5
+-- Crear un procedimiento almacenado para dar de baja a un empleado 
+-- pasándole como parámetro su apellido. 
+
+USE `db_buenasalud`;
+DROP procedure IF EXISTS `dar_baja_empleado2`;
+
+DELIMITER $$
+USE `db_buenasalud`$$
+CREATE PROCEDURE `dar_baja_empleado2`(IN _apell VARCHAR(150))
+BEGIN
+	SET @id_empleado=0;
+    
+    SELECT emp_no INTO @id_empleado FROM empleado WHERE apellido= _apell;
+    
+    update empleado set fecha_alt=(CURRENT_DATE) WHERE emp_no = @id_empleado;
+    
+    SELECT emp_no, apellido, fecha_alt, oficio FROM empleado WHERE emp_no = @id_empleado;
+    
+END$$
